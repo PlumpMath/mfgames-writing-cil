7bb4eaf98c6cd0295a35c47b985a54554241c896
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2012-11-04 21:47:20 -0600

    Initial checkin of the project structure and stubbed out code.

---
+++ src/MfGames.Writing.Cli/CliEntry.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2012 Moonfire Games
+// Released under the MIT license
+// http://mfgames.com/mfgames-writing/license
+
+namespace MfGames.Writing.Cli
+{
+	/// <summary>
+	/// Defines the entry point for the command-line interface for MfGames Writing. Billy bob lkjsdf lksjdf lksdjf lskdfj lskdfj slkdfj sdlfkj dsf
+	/// </summary>
+	internal class CliEntry
+	{
+		/// <summary>
+		/// Starting point for the application.
+		/// </summary>
+		/// <param name="args"> The args. </param>
+		private static void Main(string[] args)
+		{
+		}
+	}
+}
