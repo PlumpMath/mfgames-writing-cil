fd2d362afa8a88af30601f37428c6df5490bae11
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2012-11-08 18:02:31 -0600

    Work on getting the gathered media references to be updated in the resulting XML.

--- src/MfGames.Writing/Xml/DocBookGatheringReader.cs
+++ src/MfGames.Writing/Xml/DocBookGatheringReader.cs
@@ -3,6 +3,7 @@
 // http://mfgames.com/mfgames-writing/license
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using MfGames.Extensions.System;
@@ -20,13 +21,166 @@ namespace MfGames.Writing.Xml
 		#region Properties
 
 		/// <summary>
+		/// When overridden in a derived class, gets the number of attributes on the current node.
+		/// </summary>
+		/// <returns>
+		/// The number of attributes on the current node.
+		///   </returns>
+		public override int AttributeCount
+		{
+			get
+			{
+				if (overriddenAttributes != null)
+				{
+					return overriddenAttributes.Count;
+				}
+
+				return base.AttributeCount;
+			}
+		}
+
+		/// <summary>
 		/// Gets or sets the output directory for copying media files.
 		/// </summary>
 		public DirectoryInfo OutputDirectory { get; set; }
 
+		/// <summary>
+		/// When overridden in a derived class, gets the text value of the current node.
+		/// </summary>
+		/// <returns>
+		/// The value returned depends on the <see cref="P:System.Xml.XmlReader.NodeType"/> of the node. The following table lists node types that have a value to return. All other node types return String.Empty.
+		/// Node type
+		/// Value
+		/// Attribute
+		/// The value of the attribute.
+		/// CDATA
+		/// The content of the CDATA section.
+		/// Comment
+		/// The content of the comment.
+		/// DocumentType
+		/// The internal subset.
+		/// ProcessingInstruction
+		/// The entire content, excluding the target.
+		/// SignificantWhitespace
+		/// The white space between markup in a mixed content model.
+		/// Text
+		/// The content of the text node.
+		/// Whitespace
+		/// The white space between markup.
+		/// XmlDeclaration
+		/// The content of the declaration.
+		///   </returns>
+		public override string Value
+		{
+			get
+			{
+				// If we are reading an attribute and we have an override, we
+				// need to use that value instead.
+				if (NodeType == XmlNodeType.Attribute
+					&& overriddenAttributes != null)
+				{
+					// Pull out that element.
+					Tuple<string, string> item = overriddenAttributes[currentAttribute];
+					return item.Item2;
+				}
+
+				return base.Value;
+			}
+		}
+
 		#endregion
 
 		#region Methods
+
+		/// <summary>
+		/// When overridden in a derived class, gets the value of the attribute with the specified index.
+		/// </summary>
+		/// <param name="i">The index of the attribute. The index is zero-based. (The first attribute has index 0.)</param>
+		/// <returns>
+		/// The value of the specified attribute. This method does not move the reader.
+		/// </returns>
+		/// <exception cref="T:System.ArgumentOutOfRangeException">
+		///   <paramref name="i"/> is out of range. It must be non-negative and less than the size of the attribute collection.
+		///   </exception>
+		public override string GetAttribute(int i)
+		{
+			if (overriddenAttributes != null)
+			{
+				return overriddenAttributes[i].Item2;
+			}
+
+			return base.GetAttribute(i);
+		}
+
+		/// <summary>
+		/// When overridden in a derived class, gets the value of the attribute with the specified <see cref="P:System.Xml.XmlReader.Name"/>.
+		/// </summary>
+		/// <param name="name">The qualified name of the attribute.</param>
+		/// <returns>
+		/// The value of the specified attribute. If the attribute is not found or the value is String.Empty, null is returned.
+		/// </returns>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="name"/> is null.
+		///   </exception>
+		public override string GetAttribute(string name)
+		{
+			if (overriddenAttributes != null)
+			{
+				foreach (Tuple<string, string> attribute in overriddenAttributes)
+				{
+					if (attribute.Item1 == name)
+					{
+						return attribute.Item2;
+					}
+				}
+
+				return null;
+			}
+
+			return base.GetAttribute(name);
+		}
+
+		/// <summary>
+		/// When overridden in a derived class, gets the value of the attribute with the specified <see cref="P:System.Xml.XmlReader.LocalName"/> and <see cref="P:System.Xml.XmlReader.NamespaceURI"/>.
+		/// </summary>
+		/// <param name="name">The local name of the attribute.</param>
+		/// <param name="namespaceURI">The namespace URI of the attribute.</param>
+		/// <returns>
+		/// The value of the specified attribute. If the attribute is not found or the value is String.Empty, null is returned. This method does not move the reader.
+		/// </returns>
+		/// <exception cref="T:System.ArgumentNullException">
+		///   <paramref name="name"/> is null.
+		///   </exception>
+		public override string GetAttribute(
+			string name,
+			string namespaceURI)
+		{
+			return GetAttribute(name);
+		}
+
+		/// <summary>
+		/// When overridden in a derived class, moves to the first attribute.
+		/// </summary>
+		/// <returns>
+		/// true if an attribute exists (the reader moves to the first attribute); otherwise, false (the position of the reader does not change).
+		/// </returns>
+		public override bool MoveToFirstAttribute()
+		{
+			currentAttribute = 0;
+			return base.MoveToFirstAttribute();
+		}
+
+		/// <summary>
+		/// When overridden in a derived class, moves to the next attribute.
+		/// </summary>
+		/// <returns>
+		/// true if there is a next attribute; false if there are no more attributes.
+		/// </returns>
+		public override bool MoveToNextAttribute()
+		{
+			currentAttribute++;
+			return base.MoveToNextAttribute();
+		}
 
 		/// <summary>
 		/// When overridden in a derived class, reads the next node from the stream.
@@ -39,7 +193,10 @@ namespace MfGames.Writing.Xml
 		///   </exception>
 		public override bool Read()
 		{
-			// Attemp to read from the underlying reader.
+			// Reset the overriden attributes so we use the underyling version.
+			overriddenAttributes = null;
+
+			// Attempt to read from the underlying reader.
 			bool read = base.Read();
 
 			if (!read)
@@ -172,7 +329,7 @@ namespace MfGames.Writing.Xml
 			FileInfo sourceFile,
 			FileInfo outputFile)
 		{
-			// TODO Update the attribute.
+			// Copy the file into the appropriate location.
 			sourceFile.CopyTo(outputFile.FullName);
 
 			// Update the attributes.
@@ -186,6 +343,28 @@ namespace MfGames.Writing.Xml
 		/// <param name="file">The file.</param>
 		private void UpdateFileAttribute(FileInfo file)
 		{
+			// Create the overriden attributes list so we use that instead.
+			overriddenAttributes = new List<Tuple<string, string>>();
+
+			// Loop through all the attributes, grabbing their name and values.
+			MoveToFirstAttribute();
+
+			do
+			{
+				// Grab the next attribute, unless it is the fileref.
+				var entry = new Tuple<string, string>(
+					Name,
+					Name == "fileref"
+						? file.FullName
+						: base.Value);
+
+				// Add the new attribute to the list.
+				overriddenAttributes.Add(entry);
+			}
+			while (MoveToNextAttribute());
+
+			// Move back to the beginning of the element.
+			MoveToElement();
 		}
 
 		#endregion
@@ -200,6 +379,13 @@ namespace MfGames.Writing.Xml
 			: base(underlyingReader)
 		{
 		}
+
+		#endregion
+
+		#region Fields
+
+		private int currentAttribute;
+		private List<Tuple<string, string>> overriddenAttributes;
 
 		#endregion
 	}
